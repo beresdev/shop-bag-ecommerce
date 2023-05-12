7701b2647417e807b7155f96f7588659a02e0624
beresdev <beresdev@gmail.com>
2023-05-12 18:15:39 -0500

    Consulta de productos a API #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react"
 
 function App() {
+  const [products, setProducts] = useState([]);
+  let productsCopy = [...products];
+
+  useEffect (() => {
+    fetch('https://dummyjson.com/products?limit=100')
+      .then(res => res.json())
+      .then(data => setProducts(data.products))
+  }, [])
+
+  //console.log(productsCopy)
   return (
     <>
     <h1>¡Hola mundo!</h1>
